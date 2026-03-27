7a9874c2c61f6f2b4a140ae75c80d933510a90ab
Vinny <163838894+VinnYuri@users.noreply.github.com>
2026-03-27 11:29:53 +0100

    Add files via upload

---
+++ php/changePassword.php
@@ -0,0 +1,32 @@
+<?php 
+    include_once 'db.php';
+    header('Content-Type: application/json');
+    error_reporting(E_ALL);
+
+    session_start();
+    if(!isset($_SESSION['email'])){
+        header("Location: ../index.html");
+        exit();
+    }
+
+    $db = new database();
+    if(isset($_POST['submit'])){
+        $password = $_POST['newPass'];
+        $confermPass = $_POST['confPass'];
+
+        if($password !== $confermPass){
+            header("Location: ../optionsPage.html?error=Passwords do not match");
+            exit();
+        }
+
+        $db->connect();
+        $modify = $db->modifyPassword('user', $password);
+
+        $db->close();
+
+        if($modify){
+            header("Location: ../optionsPage.html");
+            exit();
+        }
+    }
+?>
