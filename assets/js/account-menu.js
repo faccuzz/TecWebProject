f586a11f8a0da7c636bd08a929733e8604e27042
Davide <davidefacco12.19@gmail.com>
2026-03-27 10:39:26 +0100

    login check in progress

---
+++ assets/js/account-menu.js
@@ -0,0 +1,76 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const accountBtn = document.getElementById('account-button');
+    const accountPopup = document.getElementById('account-popup');
+
+    if (!accountBtn || !accountPopup) return;
+
+    //Elementi clicabili all'interno del popup
+    //firstElement === closePopupButton, distinzione: firstElement x posizione, closePopupButton x funzione
+    let focusableElements, firstElement, lastElement, closePopupBtn;
+    
+    let isUserLoggedIn = false;
+
+    async function verifyLogin() {
+        try {
+            if (data.logged_in) {
+                isUserLoggedIn = true;
+                
+                accountBtn.removeAttribute('aria-haspopup');
+                accountBtn.removeAttribute('aria-expanded');
+            } else {
+                isUserLoggedIn = false;
+            }
+
+        } catch (error) {
+            console.error("Errore nel login check:", error);
+        }
+    }
+
+    function toggleAccountMenu() {
+        const isActive = accountPopup.classList.toggle('active');
+
+        accountBtn.setAttribute('aria-expanded', isActive);
+
+        if (isActive) {
+            focusableElements[1].focus();
+        } else {
+            accountBtn.focus();
+        }
+    }
+
+    accountBtn.addEventListener('click', (e) => {
+        if (isUserLoggedIn) {
+            //Se loggato apre impostazioni, altrimenti il popup
+            window.location.href = './optionsPage.html'; 
+        } else {
+            //Permette di non propagare l'evento verso l'alto, evita che il click per aprire chiuda anche il popup stesso
+            e.stopPropagation();
+            toggleAccountMenu();
+        }
+    });
+    
+
+    closePopupBtn.addEventListener('click', toggleAccountMenu);
+
+    //Chiudi cliccando fuori dal pop up
+    document.addEventListener('click', (e) => {
+        if (accountPopup.classList.contains('active') && !accountPopup.contains(e.target)) {
+            toggleAccountMenu();
+        }
+    });
+
+    //Focus Trap per accessibilità
+    accountPopup.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape') {
+            toggleAccountMenu();
+            return;
+        }
+
+        if (e.key === 'Tab') {
+            if (document.activeElement === lastElement) {
+                e.preventDefault();
+                firstElement.focus();
+            }
+        }
+    });
+});
